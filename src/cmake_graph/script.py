b3d973ec0e7684b719186b74174dea8482a70498
Alex Toldaiev <oleksii.toldaiev@gmail.com>
2025-05-21 19:17:59 +0000

    UP construct dot graph

--- src/cmake_graph/script.py
+++ src/cmake_graph/script.py
@@ -13,11 +13,39 @@ graph my_graph {
 }
 """
 
+json_example = {
+    "targets": [
+        {"name": "foo", "depends": ["bar", "baz"]},
+        {"name": "bar", "depends": []},
+        {"name": "baz", "depends": []},
+    ]
+}
 
-def cli():
+
+def example_dot():
     graphs = pydot.graph_from_dot_data(dot_example)
     graph = graphs[0]
-    graph.write_svg("example.svg")
+    graph.write_svg("example_dot.svg")
+
+
+def cli():
+    example_dot()
+
+    graph = pydot.Dot("my_graph", graph_type="digraph", bgcolor="white")
+    for tinfo in json_example["targets"]:
+        extra_info = []
+        extra_info.append(f"len(depends)={len(tinfo['depends'])}")
+
+        tname = tinfo["name"]
+        target_node = pydot.Node(tname, tooltip="\n".join(extra_info))
+        graph.add_node(target_node)
+
+        # can it add edges before other nodes are known?
+        for dep_name in tinfo["depends"]:
+            dep_edge = pydot.Edge(tname, dep_name, style="dashed")
+            graph.add_edge(dep_edge)
+
+    graph.write_svg("example_json.svg")
 
 
 if __name__ == "__main__":
