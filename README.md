cb39055f535e61a2f1693f3ed23e3860ce20a315
Alex Toldaiev <oleksii.toldaiev@gmail.com>
2025-05-23 15:55:58 +0000

    Reproducing the cmake --graphviz
    There is lots of useful info in the file-api jsons!
    
    But somehow the INTERFACE targets did not show up in the cmake_template
    example.
    
    	modified:   .gitignore
    	modified:   README.md
    	new file:   examples/targetgraph-Release_example.svg
    	modified:   pyproject.toml
    	modified:   src/cmake_graph/script.py

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 Trying out an idea to add more info to the `cmake --graphviz` graph.
+
+For example:
+```
+git clone --recursive git@github.com:cpp-best-practices/cmake_template.git
+cd cmake_template
+cmake_graph setup
+cmake -S . -B build
+...
+cmake_graph graph
+ls targetgraph-Release.svg
+```
+
+![Targets of cmake_template](./examples/targetgraph-Release.svg)
--- src/cmake_graph/script.py
+++ src/cmake_graph/script.py
@@ -6,12 +6,25 @@ import json
 import logging
 from glob import glob
 from os.path import isfile, isdir, join, getctime
+import pydot
+from collections import defaultdict
 
 logging.basicConfig(level=logging.INFO)
 
 CMAKE_API_CLIENT_NAME = "targetgraph"
 CMAKE_API_PATH = ".cmake/api/v1/"
 
+node_shapes = defaultdict(lambda: "septagon")
+node_shapes.update(
+    {
+        "STATIC_LIBRARY": "octagon",
+        "INTERFACE_LIBRARY": "pentagon",
+        "SHARED_LIBRARY": "doubleoctagon",
+        "OBJECT_LIBRARY": "hexagon",
+        "MODULE_LIBRARY": "tripleoctagon",
+        "UTILITY": "note",
+    }
+)
 
 
 def cmake_api_setup_query(build_dir: str):
@@ -32,22 +45,6 @@ def cmake_api_get_reply_dir(build_dir: str):
     return api_dir_reply
 
 
-def cmake_api_get_reply_file(reply_dir: str):
-    # find the latest index
-    paths = glob(join(reply_dir, "index*"))
-    files = [p for p in paths if isfile(p)]
-    index_file = max(files, key=getctime)
-
-    with open(index_file, "r") as f:
-        index = json.load(f)
-
-    my_reply = index["reply"][f"client-{CMAKE_API_CLIENT_NAME}"]
-    codemodel_fname = my_reply["codemodel-v2"]["jsonFile"]
-    full_fpath = join(reply_dir, codemodel_fname)
-    assert isfile(full_fpath), f"not a file: {full_fpath}"
-    return full_fpath
-
-
 def cmake_api_configs(codemodel_fname: str):
     assert isfile(codemodel_fname)
     with open(codemodel_fname, "r") as f:
@@ -62,6 +59,100 @@ def cmake_api_projects_directories_targets(codemodel_cfg: dict):
         codemodel_cfg["directories"],
         codemodel_cfg["targets"],
     )
+
+
+class Target:
+    def __init__(self, json_fpath, project=None, directory=None):
+        assert isfile(json_fpath)
+        with open(json_fpath, "r") as f:
+            self._json = json.load(f)
+
+        self.project = project
+        self.directory = directory
+
+    def type(self):
+        return self._json["type"]
+
+    def sources(self):
+        return [src["path"] for src in self._json.get("sources", [])]
+
+    def cmake_lists(self):
+        return self._json["backtraceGraph"]["files"]
+
+    def dependency_ids(self):
+        return [dep["id"] for dep in self._json.get("dependencies", [])]
+
+    def target_id(self):
+        return self._json["id"]
+
+    def target_name(self):
+        return self._json["name"]
+
+
+def cmake_build_config_graph(config: dict, reply_dir: str):
+    cfg_name = config["name"]
+    projects = config["projects"]
+    directories = config["directories"]
+    targets = config["targets"]
+
+    targets_dict = {t_cfg["id"]: t_cfg for t_cfg in targets}
+
+    graph = pydot.Dot(f"targetgraph-{cfg_name}", graph_type="digraph", bgcolor="white")
+
+    for t_cfg in targets:
+        directory = directories[t_cfg["directoryIndex"]]
+        project = projects[t_cfg["projectIndex"]]
+
+        t_json = t_cfg["jsonFile"]
+        target = Target(join(reply_dir, t_json), project=project, directory=directory)
+
+        # create the node and dependencies
+        extra_info = []
+        extra_info.append(f"type={target.type()}")
+        extra_info.append(f"len(depends)={len(target.dependency_ids())}")
+        extra_info.append(f"sources={'\n'.join(target.sources())}")
+
+        t_name = target.target_name()
+        target_node = pydot.Node(t_name, tooltip="\n".join(extra_info))
+        if target.type() != "EXECUTABLE":
+            target_node.set_shape(node_shapes[target.type()])
+        graph.add_node(target_node)
+
+        # can it add edges before other nodes are known?
+        for t_id in target.dependency_ids():
+            dep_name = targets_dict[t_id]["name"]
+            dep_edge = pydot.Edge(t_name, dep_name, style="dashed")
+            graph.add_edge(dep_edge)
+
+    return graph
+
+
+def cmake_api_process_reply(reply_dir: str):
+    """cmake_api_process_reply(reply_dir: str)
+
+    return graphs for all configurations returned by the codemodel-v2
+    """
+
+    # find the latest index
+    paths = glob(join(reply_dir, "index*"))
+    files = [p for p in paths if isfile(p)]
+    index_file = max(files, key=getctime)
+
+    with open(index_file, "r") as f:
+        index = json.load(f)
+
+    my_reply = index["reply"][f"client-{CMAKE_API_CLIENT_NAME}"]
+    codemodel_fname = my_reply["codemodel-v2"]["jsonFile"]
+    full_fpath = join(reply_dir, codemodel_fname)
+    assert isfile(full_fpath), f"not a file: {full_fpath}"
+
+    # return target graphs for each config
+    all_graphs = []
+    for cfg in cmake_api_configs(full_fpath):
+        graph = cmake_build_config_graph(cfg, reply_dir)
+        all_graphs.append(graph)
+
+    return all_graphs
 
 
 def cmake_graph_cli():
@@ -99,9 +190,10 @@ def cmake_graph_cli():
 
     # make the graph from the reply
     reply_dir = cmake_api_get_reply_dir(args.build)
-    reply_codemodel_file = cmake_api_get_reply_file(reply_dir)
-    for cfg in cmake_api_configs(reply_codemodel_file):
-        print(cmake_api_projects_directories_targets(cfg))
+    all_cfg_graphs = cmake_api_process_reply(reply_dir)
+    for graph in all_cfg_graphs:
+        graph.write_svg(f"{graph.get_name()}.svg")
+
 
 if __name__ == "__main__":
     cmake_graph_cli()
