c73051092641a669d0428fab976ba48853f5b174
Alex Toldaiev <oleksii.toldaiev@gmail.com>
2025-05-23 12:24:41 +0000

    Unpacking CMake file-api
    Now to make a graph of projects, directories and targets.
    
    	modified:   .gitignore
    	modified:   pyproject.toml
    	new file:   src/cmake_graph/dot_example.py
    	modified:   src/cmake_graph/script.py

---
+++ src/cmake_graph/dot_example.py
@@ -0,0 +1,42 @@
+import pydot
+
+dot_example = """
+graph my_graph {
+    bgcolor="white";
+    a [label="Foo2", tooltip="tools 🥂", comment = "com"];
+    b [shape=circle];
+    a -- b -- c [color=blue];
+}
+"""
+
+json_example = {
+    "targets": [
+        {"name": "foo", "depends": ["bar", "baz"]},
+        {"name": "bar", "depends": []},
+        {"name": "baz", "depends": []},
+    ]
+}
+
+def example_dot():
+    graphs = pydot.graph_from_dot_data(dot_example)
+    graph = graphs[0]
+    graph.write_svg("example_dot.svg")
+
+example_dot()
+
+graph = pydot.Dot("my_graph", graph_type="digraph", bgcolor="white")
+for tinfo in json_example["targets"]:
+    extra_info = []
+    extra_info.append(f"len(depends)={len(tinfo['depends'])}")
+
+    tname = tinfo["name"]
+    target_node = pydot.Node(tname, tooltip="\n".join(extra_info))
+    graph.add_node(target_node)
+
+    # can it add edges before other nodes are known?
+    for dep_name in tinfo["depends"]:
+        dep_edge = pydot.Edge(tname, dep_name, style="dashed")
+        graph.add_edge(dep_edge)
+
+graph.write_svg("example_json.svg")
+
--- src/cmake_graph/script.py
+++ src/cmake_graph/script.py
@@ -2,51 +2,106 @@
 Let's check how pydot works.
 """
 
-import pydot
+import json
+import logging
+from glob import glob
+from os.path import isfile, isdir, join, getctime
 
-dot_example = """
-graph my_graph {
-    bgcolor="white";
-    a [label="Foo2", tooltip="tools 🥂", comment = "com"];
-    b [shape=circle];
-    a -- b -- c [color=blue];
-}
-"""
+logging.basicConfig(level=logging.INFO)
 
-json_example = {
-    "targets": [
-        {"name": "foo", "depends": ["bar", "baz"]},
-        {"name": "bar", "depends": []},
-        {"name": "baz", "depends": []},
-    ]
-}
+CMAKE_API_CLIENT_NAME = "targetgraph"
+CMAKE_API_PATH = ".cmake/api/v1/"
 
 
-def example_dot():
-    graphs = pydot.graph_from_dot_data(dot_example)
-    graph = graphs[0]
-    graph.write_svg("example_dot.svg")
+
+def cmake_api_setup_query(build_dir: str):
+    from os import makedirs
+
+    api_dir = join(build_dir, CMAKE_API_PATH)
+    makedirs(api_dir, exist_ok=True)
+    api_dir_query = join(api_dir, "query", f"client-{CMAKE_API_CLIENT_NAME}")
+    makedirs(api_dir_query, exist_ok=True)
+    # touch the query file for codemodel-v2 object
+    with open(join(api_dir_query, "codemodel-v2"), "w") as _:
+        pass
 
 
-def cli():
-    example_dot()
+def cmake_api_get_reply_dir(build_dir: str):
+    api_dir_reply = join(build_dir, CMAKE_API_PATH, "reply")
+    assert isdir(api_dir_reply)
+    return api_dir_reply
 
-    graph = pydot.Dot("my_graph", graph_type="digraph", bgcolor="white")
-    for tinfo in json_example["targets"]:
-        extra_info = []
-        extra_info.append(f"len(depends)={len(tinfo['depends'])}")
 
-        tname = tinfo["name"]
-        target_node = pydot.Node(tname, tooltip="\n".join(extra_info))
-        graph.add_node(target_node)
+def cmake_api_get_reply_file(reply_dir: str):
+    # find the latest index
+    paths = glob(join(reply_dir, "index*"))
+    files = [p for p in paths if isfile(p)]
+    index_file = max(files, key=getctime)
 
-        # can it add edges before other nodes are known?
-        for dep_name in tinfo["depends"]:
-            dep_edge = pydot.Edge(tname, dep_name, style="dashed")
-            graph.add_edge(dep_edge)
+    with open(index_file, "r") as f:
+        index = json.load(f)
 
-    graph.write_svg("example_json.svg")
+    my_reply = index["reply"][f"client-{CMAKE_API_CLIENT_NAME}"]
+    codemodel_fname = my_reply["codemodel-v2"]["jsonFile"]
+    full_fpath = join(reply_dir, codemodel_fname)
+    assert isfile(full_fpath), f"not a file: {full_fpath}"
+    return full_fpath
 
+
+def cmake_api_configs(codemodel_fname: str):
+    assert isfile(codemodel_fname)
+    with open(codemodel_fname, "r") as f:
+        codemodel = json.load(f)
+
+    return codemodel["configurations"]
+
+
+def cmake_api_projects_directories_targets(codemodel_cfg: dict):
+    return (
+        codemodel_cfg["projects"],
+        codemodel_cfg["directories"],
+        codemodel_cfg["targets"],
+    )
+
+
+def cmake_graph_cli():
+    import argparse
+
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+        description="Graph CMake targets using file-api",
+        epilog="""Examples:\ncmake_graph -S . -B build/""",
+    )
+
+    parser.add_argument(
+        "command",
+        type=str,
+        choices=["setup", "graph"],
+        help="command to execute: setup or graph",
+    )
+    parser.add_argument(
+        "-B", "--build", default="./build/", help="build directory of a CMake project"
+    )
+    parser.add_argument(
+        "-d", "--debug", action="store_true", help="DEBUG level of logging"
+    )
+
+    args = parser.parse_args()
+
+    if args.debug:
+        logging.getLogger().setLevel(logging.DEBUG)
+
+    if args.command == "setup":
+        cmake_api_setup_query(args.build)
+        return
+
+    assert isdir(args.build)
+
+    # make the graph from the reply
+    reply_dir = cmake_api_get_reply_dir(args.build)
+    reply_codemodel_file = cmake_api_get_reply_file(reply_dir)
+    for cfg in cmake_api_configs(reply_codemodel_file):
+        print(cmake_api_projects_directories_targets(cfg))
 
 if __name__ == "__main__":
-    cli()
+    cmake_graph_cli()
